62f606aaf24eaae8e6f6f49f2cdf957683e4bcae
Lawrence <lawrence.langen@gmail.com>
2023-01-18 11:50:20 +0100

    Rechnen01

--- script_01.js
+++ script_01.js
@@ -11,15 +11,47 @@
 4. Ausgabe in Konsole : check!
 */
 
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(3,0));
+function divide(a,b){
+
+	if (b == 0) {//
+		return "Division durch 0 nicht möglich!"
+	} else {
+		return a / b;
+	}
+	
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b){
+	return a * b;
+}
+
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+function subtract(a,b) {
+	return a - b;
+}
 
 // module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
 function add(a,b) {
 	return a + b;
-	
 }
 
 
