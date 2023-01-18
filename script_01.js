286b4679eecab3c04a26c3068c741931684acc89
Lawrence <lawrence.langen@gmail.com>
2023-01-18 10:51:56 +0100

    Rechner_0

--- script_01.js
+++ script_01.js
@@ -13,6 +13,16 @@
 
 
 
+// module: addition a + b |  test:
+output(add(2,2));
+output(add(2,-2));
+output(add(2,0));
+function add(a,b) {
+	return a + b;
+	
+}
+
+
 
 // module:  output | test:
 // output("hello");
